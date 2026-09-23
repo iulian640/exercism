468bee021de63bbde71c06b0e7d8bf58e6c55aeb
Iulian Timofei <iuliantim21@gmail.com>
2026-09-23 16:35:03 +0200

    feat: add squeaky-clean solution

---
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -0,0 +1,12 @@
+class SqueakyClean {
+    static String clean(String identifier) {
+
+        char[] Array = identifier.toCharArray();
+
+        for (int i = 0; i > Array.length; i++) {
+
+        }
+        return identifier.replace(" ", "_");
+
+    }
+}
